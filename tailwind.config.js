2627105a8f213d474db9568fda535ed4400dd07e
Drashty Majmudar <drashtymajmudar@gmail.com>
2025-08-05 17:45:59 -0400

    upgrading to make React app

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // For the main HTML file (if applicable, e.g., public/index.html)
+    "./src/**/*.{js,jsx,ts,tsx}", // Scan all JS/JSX/TS/TSX files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
